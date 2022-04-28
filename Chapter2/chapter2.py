578119a2201bbd81e76c13f9d28dca7293c18775
Naftal Rainer <nrainer001@gmail.com>
2022-04-28 11:54:38 +0300

    Blur Image

--- Chapter2/chapter2.py
+++ Chapter2/chapter2.py
@@ -6,6 +6,11 @@ img = cv2.imread('..\Resources\deer.jpg')
 
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Blur Image
+
+imBlur = cv2.GaussianBlur(imgray,(7,7),0) # (7,7) is the kernel size. It should be an odd number
+
 cv2.imshow('Gray Image', imgray)
+cv2.imshow('Blurred Image', imBlur)
 
 cv2.waitKey(0)                                  
